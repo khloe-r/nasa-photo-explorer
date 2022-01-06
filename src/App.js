4411849005bec32c6186469859cb690e7675cb93
khloe-r <khloe.ramdhan@outlook.com>
2022-01-06 10:33:35 -0500

    added api call and like photo feature

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Ratio from "react-bootstrap/Ratio";
+import { Row, Col, Card, Button } from "react-bootstrap";
+import { Heart, HeartFill } from "react-bootstrap-icons";
 
 function App() {
+  const [photos, setPhotos] = useState();
+
+  async function getData() {
+    const response = await fetch(`https://api.nasa.gov/planetary/apod?count=20&api_key=${process.env.REACT_APP_NASA_APIKEY}`);
+    const data = await response.json();
+    console.log(data);
+    setPhotos(data);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>NASA Photo Explorer</h2>
+      <Row xs={1} md={4} className="g-4">
+        {photos ? (
+          photos.map((pic) => {
+            return <PhotoCard pic={pic} />;
+          })
+        ) : (
+          <p>Loading...</p>
+        )}
+      </Row>
     </div>
+  );
+}
+
+function PhotoCard(props) {
+  const [like, setLiked] = useState(false);
+
+  function likeHeart() {
+    setLiked(like ? false : true);
+  }
+
+  return (
+    <Col>
+      <Card>
+        <Card.Img aspectRatio="16x9" variant="top" src={props.pic.url} />
+        <Card.Body>
+          <Card.Title>{props.pic.title}</Card.Title>
+          <Card.Text>{props.pic.explanation}</Card.Text>
+          {like ? <HeartFill className="text-danger" onClick={likeHeart} /> : <Heart onClick={likeHeart} />}
+        </Card.Body>
+      </Card>
+    </Col>
   );
 }
 
